174b35b91456f6408d7608b06cbb8fc995458dea
0minyoung0 <darkard37@gmail.com>
2023-06-13 16:03:46 +0900

    [5, 6장] 블로그 API, 화면 구성

---
+++ springboot-developer/src/main/java/me/minyoung/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package me.minyoung.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.minyoung.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
