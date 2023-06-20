8ce3a856e12eecfbedd07a7612890682bbf07ae3
0minyoung0 <darkard37@gmail.com>
2023-06-20 15:13:44 +0900

    [10장] OAuth2로 로그인/로그아웃 구현하기

--- src/main/java/me/minyoung/springbootdeveloper/domain/User.java
+++ src/main/java/me/minyoung/springbootdeveloper/domain/User.java
@@ -26,15 +26,18 @@ public class User implements UserDetails {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -69,5 +72,12 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
